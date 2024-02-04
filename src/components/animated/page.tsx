ec2d16fa84991fcb6b45ffd025331d73548007bc
Ognjen Mišić <ognjen.misic@oradian.com>
2024-02-04 21:56:42 +0100

    finish up file download feature on all 3 platforms

--- src/components/animated/page.tsx
+++ src/components/animated/page.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
+import { cn } from "@/lib/utils";
+
 const animations = {
   initial: { opacity: 0, x: -100 },
   animate: { opacity: 1, x: 0 },
@@ -10,20 +12,27 @@ const animations = {
 type AnimatedPageProps = {
   forward?: boolean;
   children: ReactNode;
+  className?: string;
 };
 
-export const AnimatedPage = ({ forward, children }:  AnimatedPageProps) => {
+export const AnimatedPage = ({
+  forward,
+  children,
+  className,
+}: AnimatedPageProps) => {
   return (
     <motion.div
       variants={{
-        initial: forward ? { ...animations.initial, x: 100 } : animations.initial,
+        initial: forward
+          ? { ...animations.initial, x: 100 }
+          : animations.initial,
         animate: animations.animate,
         exit: forward ? { ...animations.exit, x: 100 } : animations.exit,
       }}
       initial="initial"
       animate="animate"
       exit="exit"
-      className="overflow-hidden"
+      className={cn("overflow-hidden", className)}
       transition={{ duration: 0.3 }}>
       {children}
     </motion.div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,70 @@
 import { clsx, type ClassValue } from "clsx";
 import { format } from "date-fns";
 import { twMerge } from "tailwind-merge";
+import { Capacitor } from "@capacitor/core";
+import { Filesystem, Directory } from "@capacitor/filesystem";
 
 import { Payslip } from "./types";
+
+const convertImageToBase64 = async (file: string) => {
+  try {
+    const response = await fetch(import.meta.env.BASE_URL + file);
+    const blob = await response.blob();
+    const base64String = await convertBlobToBase64(blob);
+    return base64String;
+  } catch (error) {
+    throw new Error("Error fetching or converting image to base64");
+  }
+};
+
+const convertBlobToBase64 = (blob: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (!reader.result) {
+        return reject("No result");
+      }
+
+      resolve(reader.result as string);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(blob);
+  });
+};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const handleDownload = (payslip?: Payslip) => {
+export const handleDownload = async (payslip?: Payslip) => {
   if (!payslip) return;
 
   const { file, fromDate } = payslip;
+  const outputFileName = format(fromDate, "MM-yyyy") + ".jpeg";
 
-  const link = document.createElement("a");
-  link.href = `${file}`;
-  link.download = format(fromDate, "MM-yyyy") + ".jpeg";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+  if (Capacitor.isNativePlatform()) {
+    try {
+      const base64 = await convertImageToBase64(file.slice(1));
+      await Filesystem.writeFile({
+        recursive: true,
+        path: outputFileName,
+        data: base64,
+        directory: Directory.Documents,
+      });
+    } catch (error) {
+      console.error("write error", error);
+    }
+  } else {
+    const link = document.createElement("a");
+    link.href = file;
+    link.download = outputFileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 };
